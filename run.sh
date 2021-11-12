b281b04462db4f9a12e55c42a6e8826d6f6cb22e
Shane <jonas.shane@gmail.com>
2021-11-12 14:37:00 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,8 +31,8 @@ fi
 git worktree add "${DIRECTORY}" gh-pages
 yarn "${NPM_COMMAND}"
 cd "${DIRECTORY}"
-cp -r . ${{ runner.temp }}/${NEW_VERSION}/
-cp -r ${{ runner.temp }}/${NEW_VERSION}/ ${NEW_VERSION}/
+cp -r . ${TMP}/${NEW_VERSION}/
+cp -r ${TMP}/${NEW_VERSION}/ ${NEW_VERSION}/
 git add --all
 git commit -m "gh-pages deploy - ${NEW_VERSION}"
 git push -f origin gh-pages
