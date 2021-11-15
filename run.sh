f71db41edd09ad5c4dd11410b37fa72ae8d5bd3d
shanejonas <jonas.shane@gmail.com>
2021-11-15 15:50:06 -0800

    Changed run.sh to use `gh-pages` npm package

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ DIRECTORY="${1}"
 CURRENT_BRANCH=$(git branch --show-current)
 PREFIX=${2}
 NPM_COMMAND=${3}
+DESTINATION=${4}
 NEW_VERSION="${GITHUB_HEAD_REF#$PREFIX}"
 
 if [[ -z $DIRECTORY ]]; then
@@ -15,25 +16,13 @@ if [[ -z $DIRECTORY ]]; then
   exit 1
 fi
 
+if [[ -z $DESTINATION ]]; then
+  echo "Error: No Destination specified."
+  exit 1
+fi
+
 git config user.name github-actions
 git config user.email github-actions@github.com
 
-if git checkout --orphan gh-pages
-  then
-    git reset --hard
-    git commit --allow-empty -m "Initial gh-pages commit"
-    git checkout "${CURRENT_BRANCH}"
-    echo "Created branch gh-pages"
-  else
-    echo "gh-pages branch already created"
-fi
-
-git worktree add "${DIRECTORY}" gh-pages
 yarn "${NPM_COMMAND}"
-cd "${DIRECTORY}"
-TMP_PATH=$(mktemp -d)
-cp -r . ${TMP_PATH}/${NEW_VERSION}/
-cp -r ${TMP_PATH}/${NEW_VERSION}/ ${NEW_VERSION}/
-git add --all
-git commit -m "gh-pages deploy - ${NEW_VERSION}"
-git push -f origin gh-pages
+yarn gh-pages --dist "${DIRECTORY}" --message "gh-pages deploy - ${NEW_VERSION}" || abort "gh-pages failed to deploy
