27fca615437a945520b0f1c53175fc55c179e26e
shanejonas <jonas.shane@gmail.com>
2021-11-15 17:43:36 -0800

    Fixed git user settings + remove yarn.lock

--- run.sh
+++ run.sh
@@ -21,6 +21,9 @@ if [[ -z $DESTINATION ]]; then
   exit 1
 fi
 
+git config user.name github-actions
+git config user.email github-actions@github.com
+
 yarn "${NPM_COMMAND}"
 git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 npx gh-pages --dist "${DIRECTORY}" --message "gh-pages deploy - ${NEW_VERSION}" || abort "gh-pages failed to deploy"
