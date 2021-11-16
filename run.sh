23363f8e631b882257c2f0169fbf5142cdcfa2bf
Erik Marks <rekmarks@protonmail.com>
2021-11-15 23:10:42 -0800

    New attempt at versioned deployment fix

--- run.sh
+++ run.sh
@@ -4,26 +4,61 @@ set -x
 set -e
 set -o pipefail
 
-DIRECTORY="${1}"
-CURRENT_BRANCH=$(git branch --show-current)
-PREFIX=${2}
-NPM_COMMAND=${3}
-DESTINATION=${4}
-NEW_VERSION="${GITHUB_HEAD_REF#$PREFIX}"
+SOURCE_DIRECTORY="${1}"
+RELEASE_BRANCH_PREFIX="${2}"
+PACKAGE_BUILD_COMMAND="${3}"
+DESTINATION_DIRECTORY="${4}"
 
-if [[ -z $DIRECTORY ]]; then
-  echo "Error: No Directory specified."
+if [[ -z $SOURCE_DIRECTORY ]]; then
+  echo "Error: No source directory specified."
   exit 1
 fi
 
-if [[ -z $DESTINATION ]]; then
-  echo "Error: No Destination specified."
+if [[ -z $RELEASE_BRANCH_PREFIX ]]; then
+  echo "Error: No release branch prefix specified."
   exit 1
+fi
+
+if [[ -z $PACKAGE_BUILD_COMMAND ]]; then
+  echo "Error: No build command name specified."
+  exit 1
+fi
+
+if [[ -z $DESTINATION_DIRECTORY ]]; then
+  echo "Error: No destination directory specified."
+  exit 1
+fi
+
+NEW_VERSION="${GITHUB_HEAD_REF#"$RELEASE_BRANCH_PREFIX"}"
+
+if [[ -z $NEW_VERSION ]]; then
+  echo "Error: Failed to extract new version from release branch name."
+  exit 1
+fi
+
+if [[ -z $NEW_VERSION ]]; then
+  echo "Error: Failed to extract new version from release branch name."
+  exit 1
+fi
+
+# If publishing directly to the root directory, erase all existing files on the
+# gh-pages branch. Otherwise, do not modify any files outside of the destination
+# directory.
+if [[ "$DESTINATION_DIRECTORY" = "." ]]; then
+  ADD=""
+else
+  # "--add" prevents existing files from being deleted.
+  ADD="--add"
 fi
 
 git config user.name github-actions
 git config user.email github-actions@github.com
 
-yarn "${NPM_COMMAND}"
-git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
-npx gh-pages -a --dist "${DIRECTORY}" --dest "${DESTINATION}" --message "gh-pages deploy - ${NEW_VERSION}"
+yarn "${PACKAGE_BUILD_COMMAND}"
+git remote set-url origin "https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+npx gh-pages@3.2.3 \
+  "$ADD" \
+  --dist "${SOURCE_DIRECTORY}" \
+  --dest "${DESTINATION_DIRECTORY}" \
+  --message "gh-pages deploy - ${NEW_VERSION}"
