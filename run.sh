6100324da088e7cc7f00be4fa877c1a3ae601384
Shane <jonas.shane@gmail.com>
2021-11-16 22:40:10 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -57,9 +57,9 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 
 yarn "${PACKAGE_BUILD_COMMAND}"
-exists_in_remote=$(git ls-remote --heads origin gh-pages)
+exists_in_remote=$(git ls-remote --exit-code . origin/gh-pages)
 
-if [[ -z ${exists_in_remote} ]]; then
+if [[ -z "${exists_in_remote}" ]]; then
   echo "gh-pages branch already created"
 else
   git checkout --orphan gh-pages
