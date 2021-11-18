975e8b8d3baec4316c42718f27391cf05731c1ef
Erik Marks <rekmarks@protonmail.com>
2021-11-17 16:38:56 -0800

    Fix formatting issues

--- run.sh
+++ run.sh
@@ -30,7 +30,6 @@ if [[ -z $COMMIT_MESSAGE ]]; then
   exit 1
 fi
 
-
 # If publishing directly to the root directory, erase all existing files on the
 # gh-pages branch. Otherwise, do not modify any files outside of the destination
 # directory.
@@ -47,27 +46,25 @@ git config user.name github-actions
 git config user.email github-actions@github.com
 git remote set-url origin "$REPO"
 
-branch_exists=$(git ls-remote --quiet --heads "$REPO" gh-pages)
+BRANCH_EXISTS=$(git ls-remote --quiet --heads "$REPO" gh-pages)
 
-if [[ -n "$branch_exists" ]]
-  then
-    echo "gh-pages branch already created"
-  else
-    git checkout --orphan gh-pages
-    git reset --hard
+if [[ -n "$BRANCH_EXISTS" ]]; then
+  echo "gh-pages branch already created"
+else
+  git checkout --orphan gh-pages
+  git reset --hard
 
-    git commit --allow-empty -m "Initial gh-pages commit"
-    git push origin gh-pages
-    git checkout "${CURRENT_BRANCH}"
-    echo "Created branch gh-pages"  
+  git commit --allow-empty -m "Initial gh-pages commit"
+  git push origin gh-pages
+  git checkout "${CURRENT_BRANCH}"
+  echo "Created branch gh-pages"  
 fi
 
-if [[ "$ADD" = "--add" ]]
-  then
-    # Edit package.json in place to reflect the new homepage url
-    # This is required by some build tools, e.g. Webpack
-    npx json -I -f package.json \
-      -e "this.homepage=(this.homepage.replace(/\/$/u, '')) + \"/${DESTINATION_DIRECTORY}/\""
+if [[ "$ADD" = "--add" ]]; then
+  # Edit package.json in place to reflect the new homepage url
+  # This is required by some build tools, e.g. Webpack
+  npx json -I -f package.json \
+    -e "this.homepage=(this.homepage.replace(/\/$/u, '')) + \"/${DESTINATION_DIRECTORY}/\""
 fi
 
 yarn "${PACKAGE_BUILD_COMMAND}"
