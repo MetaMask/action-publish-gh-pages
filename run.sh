4f8c6da04b68aa632712d7602cf4134be17762d2
Shane <jonas.shane@gmail.com>
2021-11-17 17:01:07 -0800

    Added Version (#16)
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Changed run.sh to use `gh-pages` npm package
    
    * Fixed destination
    
    * Fixed matching quote
    
    * Changed yarn to npx for gh-pages command
    
    * Removed git user info
    
    * Fixed remote for gh-pages command
    
    * Fixed git user settings + remove yarn.lock
    
    * Added dest param to gh-pages
    
    * Added -a for only additive gh-pages releases
    
    * New attempt at versioned deployment fix
    
    * Update run.sh
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * Update action.yml
    
    * Update run.sh
    
    * Update action.yml
    
    * Update run.sh
    
    * Update run.sh
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * Update run.sh
    
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>
    
    * Fix formatting issues
    
    * Update action.yml
    
    * Fix docs, remove unused input
    
    Co-authored-by: Erik Marks <rekmarks@protonmail.com>
    Co-authored-by: Erik Marks <25517051+rekmarks@users.noreply.github.com>

--- run.sh
+++ run.sh
@@ -4,33 +4,73 @@ set -x
 set -e
 set -o pipefail
 
-DIRECTORY="${1}"
+SOURCE_DIRECTORY="${1}"
+PACKAGE_BUILD_COMMAND="${2}"
+DESTINATION_DIRECTORY="${3}"
+COMMIT_MESSAGE="${4}"
 CURRENT_BRANCH=$(git branch --show-current)
-PREFIX=${2}
-NPM_COMMAND=${3}
-NEW_VERSION="${GITHUB_HEAD_REF#$PREFIX}"
 
-if [[ -z $DIRECTORY ]]; then
-  echo "Error: No Directory specified."
+if [[ -z $SOURCE_DIRECTORY ]]; then
+  echo "Error: No source directory specified."
   exit 1
 fi
 
-git config user.name github-actions
-git config user.email github-actions@github.com
-
-if git checkout --orphan gh-pages
-  then
-    git reset --hard
-    git commit --allow-empty -m "Initial gh-pages commit"
-    git checkout "${CURRENT_BRANCH}"
-    echo "Created branch gh-pages"
-  else
-    echo "gh-pages branch already created"
+if [[ -z $PACKAGE_BUILD_COMMAND ]]; then
+  echo "Error: No build command name specified."
+  exit 1
 fi
 
-git worktree add "${DIRECTORY}" gh-pages
-yarn "${NPM_COMMAND}"
-cd "${DIRECTORY}"
-git add --all
-git commit -m "gh-pages deploy - ${NEW_VERSION}"
-git push -f origin gh-pages
+if [[ -z $DESTINATION_DIRECTORY ]]; then
+  echo "Error: No destination directory specified."
+  exit 1
+fi
+
+if [[ -z $COMMIT_MESSAGE ]]; then
+  echo "Error: No commit message specified."
+  exit 1
+fi
+
+# If publishing directly to the root directory, erase all existing files on the
+# gh-pages branch. Otherwise, do not modify any files outside of the destination
+# directory.
+if [[ "$DESTINATION_DIRECTORY" = "." ]]; then
+  ADD=""
+else
+  # "--add" prevents existing files from being deleted.
+  ADD="--add"
+fi
+
+REPO="https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+git remote set-url origin "$REPO"
+
+BRANCH_EXISTS=$(git ls-remote --quiet --heads "$REPO" gh-pages)
+
+if [[ -n "$BRANCH_EXISTS" ]]; then
+  echo "gh-pages branch already created"
+else
+  git checkout --orphan gh-pages
+  git reset --hard
+
+  git commit --allow-empty -m "Initial gh-pages commit"
+  git push origin gh-pages
+  git checkout "${CURRENT_BRANCH}"
+  echo "Created branch gh-pages"  
+fi
+
+if [[ "$ADD" = "--add" ]]; then
+  # Edit package.json in place to reflect the new homepage url
+  # This is required by some build tools, e.g. Webpack
+  npx json -I -f package.json \
+    -e "this.homepage=(this.homepage.replace(/\/$/u, '')) + \"/${DESTINATION_DIRECTORY}/\""
+fi
+
+yarn "${PACKAGE_BUILD_COMMAND}"
+
+npx gh-pages@3.2.3 \
+  "$ADD" \
+  --dist "${SOURCE_DIRECTORY}" \
+  --dest "${DESTINATION_DIRECTORY}" \
+  --message "${COMMIT_MESSAGE}"
